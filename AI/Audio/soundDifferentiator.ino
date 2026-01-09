d61473cd385d7ca911719a3c65d0430a23663818
Soumya Gupta <techbots.academy.th@gmail.com>
2026-01-09 11:54:48 +0530

    Arduino code

--- AI/Audio/soundDifferentiator.ino
+++ AI/Audio/soundDifferentiator.ino
@@ -8,12 +8,21 @@ void loop() {
 
     if (c == 'A') {
       // Ambulance logic
+      digitalWrite(13,HIGH);
+      digitalWrite(12,LOW);
+      digitalWrite(11,LOW);
     }
     else if (c == 'H') {
       // Horn logic
+      digitalWrite(13,LOW);
+      digitalWrite(12,HIGH);
+      digitalWrite(11,LOW);
     }
     else if (c == 'B') {
       // Background logic
+      digitalWrite(13,LOW);
+      digitalWrite(12,LOW);
+      digitalWrite(11,HIGH);
     }
   }
 }
