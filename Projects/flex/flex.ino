965315b47b121f94459fd5f4b2687f92bdf6ca9a
Soumya Gupta <techbots.academy.th@gmail.com>
2026-03-16 12:15:27 +0530

    flex sensor

---
+++ Projects/flex/flex.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+Servo myServo;
+
+int flexPin = A0;     // Flex sensor connected to A0
+int flexValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(9);   // Servo signal pin connected to pin 9
+}
+
+void loop() {
+
+  flexValue = analogRead(flexPin);   // Read flex sensor value
+  Serial.println(flexValue);
+
+  if (flexValue > 500) {
+    myServo.write(135);   // Rotate servo to 45 degrees
+  delay(1000);
+
+  } 
+  else {
+    myServo.write(0);    // Rotate servo to 0 degrees
+  delay(1000);
+  }
+
+}
